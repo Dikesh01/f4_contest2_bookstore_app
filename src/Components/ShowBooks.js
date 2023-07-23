96118e2bddce3330a01ade56bd7173aad3acdc8d
kabir33559@gmail.com <Dikesh01>
2023-07-23 09:17:44 +0530

    modification

--- src/Components/ShowBooks.js
+++ src/Components/ShowBooks.js
@@ -2,26 +2,42 @@ import React, { useEffect, useState } from "react";
 
 
 const ShowBooks = ({harryBooks,sherlockBooks}) =>{
+    let[bookDetails,setBookDetails] = useState("")
 
-    console.log(harryBooks)
-    console.log(sherlockBooks)
+    console.log(bookDetails);
+
+
+
+    // console.log(harryBooks)
+    // console.log(sherlockBooks)
 
 
     return(
-        <div className="showAll">
-            {
-                
-                harryBooks &&
-                harryBooks.map((book) =>{
-                    return(
-                            <div>
-                                <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
-                            </div>
-                    ) 
-                })
-
-                
-            }
+        <div className="booksContainer">
+            <h3>More Books</h3>
+            <div className="showAll">
+                {
+                    
+                    sherlockBooks &&
+                    sherlockBooks.map((book) =>{
+                        return(
+                                <div className="bookDetails" onClick={()=>setBookDetails(book.volumeInfo)}>
+                                    <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+                                </div>
+                        ) 
+                    })  
+                }
+                {
+                    harryBooks &&
+                    harryBooks.map((book) =>{
+                        return(
+                                <div className="bookDetails" onClick={()=>setBookDetails(book.volumeInfo)}>
+                                    <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+                                </div>
+                        ) 
+                    })
+                }
+            </div>
             
         </div>
     )
